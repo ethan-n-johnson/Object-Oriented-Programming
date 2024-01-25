c7793ad943758cc97de72ae7e9b7ad85e7c48e21
Ethan Johnson <ethansoccer111@gmail.com>
2024-01-25 10:15:14 -0600

    checkpoint

--- P02/full_credit/Note.java
+++ P02/full_credit/Note.java
@@ -1,7 +1,24 @@
+enum Pitch {
+    C, Db, D, Eb, E, F, 
+    Gb, G, Ab, A, Bb, B
+}
+
 public class Note {
-    enum Pitch {
-        C, Db, D, Eb, E, F, 
-        Gb, G, Ab, A, Bb, B
+    public Note(Pitch pitch, int octave) {
+        this.pitch = pitch;
     }
-    
+    public Note() {
+        this.pitch = null;
+    }
+    @Override
+    public String toString() {
+        if (pitch == null) {
+            return " ";
+        }
+        else {
+            return pitch + (octave != 0 ? octave + "" : "");
+        }
+    }
+    private Pitch pitch;
+    private int octave;
 }
