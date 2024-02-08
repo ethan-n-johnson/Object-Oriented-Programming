b59d1c7cf34b27f9277b90f5e3c849498ebaf83d
Ethan Johnson <ethansoccer111@gmail.com>
2024-02-07 20:28:08 -0600

    checkpoint

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -1,15 +1,37 @@
+/**
+ * Defining an order placed by a customer.
+ * @author Ethan Johnson
+ * @version 0.2
+ * @since Version 0.2
+*/
 package store;
 import java.util.ArrayList;
 
 public class Order{
+
+    /**
+     * Constructs the order object.
+     * @param Customer The customer attached to the order
+     * @since Version 0.2
+    */
     public Order(Customer customer){
         this.orderNumber = nextOrderNumber++;
         this.customer = customer;
         this.items = new ArrayList<Item>();
     }
+    /**
+     * Adds an item to the order.
+     * @param Item The item added to the order.
+     * @since Version 0.2
+    */
     public void addItem(Item item){
         items.add(item);
     }
+    /**
+     * Sums the price of all items and returns the result.
+     * @return Returns the total price.
+     * @since Version 0.2
+    */
     public int getPrice(){
         int totalPrice = 0;
         for(Item item : items){
@@ -17,9 +39,20 @@ public class Order{
         }
         return totalPrice;
     }
+    /**
+     * Provides a string representation of the order formatted like a receipt.
+     * @return The string representation of the order.
+     * @since Version 0.2
+    */
     @Override
     public String toString(){
-        return "Test";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order # ").append(orderNumber).append(" for ").append(customer.toString()).append("\n");
+        for(Item item : items){
+            sb.append(item).append("\n");
+        }
+        sb.append("Order total $   ").append(getPrice());
+        return sb.toString();
     }
     private Customer customer;
     private ArrayList<Item> items;
