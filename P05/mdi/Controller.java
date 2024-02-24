0b05f22fc024e93fcf4ec2e193c5ed1903b66042
Ethan Johnson <ethansoccer111@gmail.com>
2024-02-23 20:18:24 -0600

    checkpoint

--- P05/mdi/Controller.java
+++ P05/mdi/Controller.java
@@ -3,13 +3,22 @@ import store.*;
 import java.util.Scanner;
 public class Controller{
     public Controller(String storeName){
-        store = new Store(storeName);
-        mainMenu = new Menu();
-        this.view = View.CUSTOMERS; 
+        this.store = new Store(storeName);
         this.mainMenu = createMainMenu();
+        this.view = View.CUSTOMERS; 
         this.output = "";
         this.isRunning = true;
         this.in = new Scanner(System.in);
+
+        Customer c1 = new Customer("Prof Rice", "george.rice@uta.edu");
+        Customer c2 = new Customer("President Joe Biden", "president@whitehouse.gov");
+        Customer c3 = new Customer("The Late Queen Elizabeth II", "queen@royal.gov.uk");
+        Customer c4 = new Customer("Mark Zuckerberg", "mark.zuckerberg@facebook.com");
+        
+        store.addCustomer(c1);
+        store.addCustomer(c2);
+        store.addCustomer(c3);
+        store.addCustomer(c4);
     }
     public void mdi()
     {
@@ -17,7 +26,7 @@ public class Controller{
             try{
                 Integer i = selectFromMenu();
                 if(i == null) continue;
-                menu.run(i);
+                mainMenu.run(i);
 
             } catch(Exception e){
                 System.err.println("Invalid Input");
@@ -32,8 +41,8 @@ public class Controller{
     {
         System.out.println("Customer List:");
         store.getCustomerList();
-        System.out.println("What is your customer number?\n>");
-        int customerIndex = getInt("Select customer\n>") - 1;
+        System.out.println("What is your customer number?\n> ");
+        int customerIndex = getInt("Select customer\n> ") - 1;
         if(customerIndex < 0) {
             System.out.println("Invalid customer selection.\nPlease try again\n");
             placeOrder();
@@ -43,58 +52,58 @@ public class Controller{
         store.getProductList();
         int productIndex = 0;
         while(productIndex != -1){
-            productIndex = getInt("Select product (0 to quit)\n>") - 1;
+            productIndex = getInt("Select product (0 to quit)\n> ") - 1;
             while (productIndex < 0){
                 if(productIndex == -1) break;
                 System.out.println("Invalid product selection.");
-                productIndex = getInt("Select product (0 to quit)\n>") - 1;
+                productIndex = getInt("Select product (0 to quit)\n> ") - 1;
             }
         }
-        int quantity = getInt("Enter quantity for\n>");
+        int quantity = getInt("Enter quantity for\n> ");
         store.addToOrder(orderNumber, productIndex, quantity);
         output = "Order placed successfully.";
         view = View.ORDERS;
     }
     private void newCustomer(){
-        Customer customer = new Customer(getString("Enter the name of the customer\n>"), getString("Enter the email of the customer\n>"));
+        Customer customer = new Customer(getString("Enter the name of the customer\n> "), getString("Enter the email of the customer\n> "));
         store.addCustomer(customer);
         output = "Customer added successfully";
         view = view.CUSTOMERS;
     }
     private void newTool(){
-        Tool tool = new Tool(getString("Enter the name of the tool\n>"), getInt("Enter the price of the tool\n>"));
+        Tool tool = new Tool(getString("Enter the name of the tool\n> "), getInt("Enter the price of the tool\n> "));
         store.addProduct(tool);
         output = "Product added successfully";
         view = view.PRODUCTS;
     }
     private void newPlant(){
         boolean exposureNotAssigned = true;
-        Exposure exposure;
+        Exposure exposure = Exposure.SUN;
         do{
-            switch(getInt("Enter the level of exposure (" + Exposure.values() + ")\n>")){
+            switch(getInt("Enter the level of exposure (" + Exposure.values() + ")\n> ")){
                 case 1:
-                    exposure = exposure.SUN;
+                    exposure = Exposure.SUN;
                     exposureNotAssigned = false;
                     break;
                 case 2:
-                    exposure = exposure.PARTSUN;
+                    exposure = Exposure.PARTSUN;
                     exposureNotAssigned = false;
                     break;
                 case 3:
-                    exposure = exposure.SHADE;
+                    exposure = Exposure.SHADE;
                     exposureNotAssigned = false;
                     break;
                 default:
                     System.out.println("Invalid Exposure");
             }
         } while(exposureNotAssigned);
-        Plant plant = new Plant(getString("Enter the species of the plant\n>"), getInt("Enter the price of the plant\n>"), exposure);
+        Plant plant = new Plant(getString("Enter the species of the plant\n> "), getInt("Enter the price of the plant\n> "), exposure);
         store.addProduct(plant);
         output = "Product added successfully";
         view = view.PRODUCTS;
     }
     private void switchView(){
-        getString("Select a view (CUSTOMERS, PRODUCTS, or ORDERS)\n>");
+        getString("Select a view (CUSTOMERS, PRODUCTS, or ORDERS)\n> ");
     }
     private String getView(){
         switch(view){
@@ -108,6 +117,7 @@ public class Controller{
                 store.getOrderList();
                 break; 
         }
+        return "Invalid View";
     }
     private Integer selectFromMenu(){
         System.out.println(clearscreen + store.getName() + "\n");
@@ -116,7 +126,7 @@ public class Controller{
         getView();
         System.out.println(output);
         output = "";
-        return getInt("Select an option\n>");
+        return getInt("Select an option\n> ");
     }
     private void print(String s){
         output += s + '\n';
@@ -160,6 +170,18 @@ public class Controller{
         }
         return d; 
     }
+
+    private Menu createMainMenu() {
+        mainMenu = new Menu();
+        mainMenu.addMenuItem(new MenuItem("Exit", this::exit));
+        mainMenu.addMenuItem(new MenuItem("Place Order", this::placeOrder));
+        mainMenu.addMenuItem(new MenuItem("Welcome new Customer", this::newCustomer));
+        mainMenu.addMenuItem(new MenuItem("Define new Tool", this::newTool));
+        mainMenu.addMenuItem(new MenuItem("Define new Plant", this::newPlant));
+        mainMenu.addMenuItem(new MenuItem("Switch View", this::switchView));
+        return mainMenu;
+    }
+
     private Store store;
     private View view;
     private Menu mainMenu;
--- P05/mdi/Manga.java
+++ P05/mdi/Manga.java
@@ -1,13 +1,15 @@
 package mdi;
 public class Manga{
     public static void main(String args[]){
-        if(args[0] != ""){
-            Controller controller = new Controller(args[0]);
-            controller.mdi();
-        }
-        else{
-            Controller controller = new Controller("Generic Store");
-            controller.mdi();
-        }
+        // if(args[0] != ""){
+        //     Controller controller = new Controller(args[0]);
+        //     controller.mdi();
+        // }
+        // else{
+        //     Controller controller = new Controller("Generic Store");
+        //     controller.mdi();
+        // }
+        Controller controller = new Controller("Generic Store");
+        controller.mdi();
     }
 }
