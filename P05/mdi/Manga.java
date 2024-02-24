578be08e058a9f04f1040384c7dd52a751371df3
Ethan Johnson <ethansoccer111@gmail.com>
2024-02-23 23:38:52 -0600

    checkpoint

--- P05/mdi/Manga.java
+++ P05/mdi/Manga.java
@@ -1,15 +1,8 @@
 package mdi;
 public class Manga{
     public static void main(String args[]){
-        // if(args[0] != ""){
-        //     Controller controller = new Controller(args[0]);
-        //     controller.mdi();
-        // }
-        // else{
-        //     Controller controller = new Controller("Generic Store");
-        //     controller.mdi();
-        // }
-        Controller controller = new Controller("Generic Store");
+        String storeName = (args.length > 0) ? args[0] : "Manga Store #1";
+        Controller controller = new Controller(storeName);
         controller.mdi();
     }
 }
