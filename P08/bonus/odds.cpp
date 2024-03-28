6236c65ffd55b05f30847824808e67c207d058e7
Ethan Johnson <ethansoccer111@gmail.com>
2024-03-28 09:48:45 -0500

    checkpoint

--- P08/bonus/odds.cpp
+++ P08/bonus/odds.cpp
@@ -4,5 +4,24 @@
 
 int main(int argc, char* argv[]){
     std::vector<std::string> odds;
-    std::vector<std::string> evens;
+    std::vector<std::string> *evens = new std::vector<std::string>();
+
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];
+        if (arg.size() % 2 != 0) odds.push_back(arg);
+        else evens->push_back(arg);
+    }
+    std::cout << "Odd lengths:" << std::endl;
+    for(std::string s: odds){
+        std::cout << s << std::endl;
+    }
+    
+    std::cout << std::endl;
+
+    std::cout << "Even lengths:" << std::endl;
+    for(std::string s: *evens){
+        std::cout << s << std::endl;
+    }
+    delete(evens);
 }
