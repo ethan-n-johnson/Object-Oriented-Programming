ae4347a01ee2d10e451792f532c53206873ff8d6
Ethan Johnson <ethansoccer111@gmail.com>
2024-03-28 08:45:49 -0500

    checkpoint

--- P08/bonus/odds.cpp
+++ P08/bonus/odds.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(int argc, char* argv[]){
-    std::vector <std::string> odds{"a","b","c"};
+    std::vector<std::string> odds;
+    std::vector<std::string> evens;
 }
