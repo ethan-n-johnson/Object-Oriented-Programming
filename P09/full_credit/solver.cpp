9e51bddf9dbc16a8c99cd4d6a58d6064c42fc9a5
Ethan Johnson <ethansoccer111@gmail.com>
2024-04-04 10:14:16 -0500

    checkpoint

--- P09/full_credit/solver.cpp
+++ P09/full_credit/solver.cpp
@@ -3,18 +3,23 @@
 #include "quadratic.h"
 #include "linear.h"
 
-int main(int argc, std::vector<double> argv){
+int main(int argc, char* argv[]){
+    std::vector<double> coefficients;
     Polynomial *p = NULL; 
-    if(argc == 3) p = new Linear(argv);
-    if(argc == 4) p = new Quadratic(argv);
+    if(argc == 3){
+        std::vector<double> coefficients = {std::stod(argv[1]), std::stod(argv[2])}; 
+        p = new Linear(coefficients);
+    }
+    if(argc == 4){
+        std::vector<double> coefficients = {std::stod(argv[1]), std::stod(argv[2]), std::stod(argv[3])}; 
+        p = new Quadratic(coefficients);
+    }
     else{
         std::cerr << "Inavlid polynomial size - must be linear or quadratic" << std::endl;
         return -1;
     }
-    p->solve();
-    int i = 0;
-    for(auto c: p){
-        p[i].solve();
-        i++;
+    std::vector<double> roots = p->solve();
+    for(auto c: roots){
+        std::cout << c << std::endl;
     }
 }
