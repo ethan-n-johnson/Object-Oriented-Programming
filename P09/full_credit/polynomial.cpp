060817cabbe724893ba5af5a047a87ad520df762
Ethan Johnson <ethansoccer111@gmail.com>
2024-04-04 09:01:13 -0500

    checkpoint

--- P09/full_credit/polynomial.cpp
+++ P09/full_credit/polynomial.cpp
@@ -6,6 +6,8 @@ class Polynomial{
     public:
         Polynomial(std::vector<double> coefficients)
             :_coefficients(coefficients) {}
+        virtual ~Polynomial();
+        virtual std::vector<double> solve() = 0;
     private:
         std::vector<double> _coefficients;
 };
