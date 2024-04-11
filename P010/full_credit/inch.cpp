696c351fcde03b82e0a2039a553c62e69e43d1fc
Ethan Johnson <ethansoccer111@gmail.com>
2024-04-11 10:29:40 -0500

    checkpoint

--- P010/full_credit/inch.cpp
+++ P010/full_credit/inch.cpp
@@ -2,6 +2,15 @@
 
 Inch::Inch(int whole, int numerator, int denominator)
         : _whole(whole), _numerator(numerator), _denominator(denominator){}
-Inch::Inch(){}
-int Inch::compare(const Inch& rhs){}
+Inch::Inch() : Inch(0,0,2){}
+int const Inch::compare(const Inch& rhs){}
 void Inch::validate(){}
+inline bool Inch::operator+(const Inch& rhs){}
+bool Inch::operator==(const Inch& rhs){return(compare(rhs) == 0);}
+bool Inch::operator!=(const Inch& rhs){return(compare(rhs) != 0);}
+bool Inch::operator<(const Inch& rhs){return(compare(rhs) < 0);}
+bool Inch::operator<=(const Inch& rhs){return(compare(rhs) <= 0);}
+bool Inch::operator>(const Inch& rhs){return(compare(rhs) > 0);}
+bool Inch::operator>=(const Inch& rhs){return(compare(rhs) >= 0);}
+std::ostream& Inch::operator<<(std::ostream& ost, Inch& inch){}
+std::ostream& Inch::operator>>(std::ostream& ost, const Inch& inch){}
--- P010/full_credit/inch.h
+++ P010/full_credit/inch.h
@@ -1,16 +1,28 @@
 #ifndef INCH_H
 #define INCH_H
 #include <iostream>
+#include <ostream>
+#include <istream>
 
 class Inch{
     public:
         Inch(int whole, int numerator, int denominator);
+        virtual ~Inch();
         Inch();
+        inline bool operator+(const Inch& rhs);
+        inline bool operator==(const Inch& rhs);
+        inline bool operator!=(const Inch& rhs);
+        inline bool operator<(const Inch& rhs);
+        inline bool operator<=(const Inch& rhs);
+        inline bool operator>(const Inch& rhs);
+        inline bool operator>=(const Inch& rhs);
+        std::ostream&  operator<<(std::ostream& ost, Inch& inch);
+        std::istream& operator>>(std::istream& ist, const Inch& inch);
     private:
         int _whole;
         int _numerator;
         int _denominator;
-        int compare(const Inch& rhs);
+        int const compare(const Inch& rhs);
         void validate();
 };
 
